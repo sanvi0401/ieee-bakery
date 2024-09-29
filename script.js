18eceeee679d810b31f4aa862a788e4b6cbdb422
sanvi0401 <sd4912@srmist.edu.in>
2024-09-29 18:37:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("orderForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    alert("Order submitted!"); // You can replace this with actual order logic
+});
